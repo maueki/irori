75a4d39ccdb043e209d98bcd7f9fcc8d62e9f001
Ueki Masaru <masaru.ueki@itage.co.jp>
2015-01-15 08:54:11 +0900

    http://golang.org/doc/articles/wiki/#tmp_4 written in goji

--- main.go
+++ main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
 
-func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+	title := c.URLParams["title"]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-	goji.Get("/hello/:name", hello)
+	goji.Get("/wiki/:title", viewHandler)
 	goji.Serve()
 }
