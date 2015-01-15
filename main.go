120dcd3e6ef594e8ec9296d1a62d3d7849f5b92a
Ueki Masaru <masaru.ueki@itage.co.jp>
2015-01-15 12:45:14 +0900

    Add edit & save handler

--- main.go
+++ main.go
@@ -34,7 +34,35 @@ func viewHandler(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+	title := c.URLParams["title"]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>" +
+		"<form action=\"/wiki/%s\" method=\"POST\">" +
+		"<textarea name=\"body\">%s</textarea><br>" +
+		"<input type=\"submit\" value=\"Save\">" +
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+	title := c.URLParams["title"]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/wiki/" + title, http.StatusFound)
+}
+
 func main() {
 	goji.Get("/wiki/:title", viewHandler)
+	goji.Get("/wiki/:title/edit", editHandler)
+	goji.Post("/wiki/:title", saveHandler)
 	goji.Serve()
 }
