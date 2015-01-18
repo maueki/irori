7dcf596d29a6e38866ebf9ad2957bef700917a12
Ueki Masaru <masaru.ueki@itage.co.jp>
2015-01-18 17:12:53 +0900

    install.sh: Add pongo2-addons for supporting markdown

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ fi
 
 go get github.com/coopernurse/gorp
 go get github.com/flosch/pongo2
+go get github.com/flosch/pongo2-addons
 go get github.com/gorilla/sessions
 go get github.com/mattn/go-sqlite3
 go get github.com/zenazn/goji
